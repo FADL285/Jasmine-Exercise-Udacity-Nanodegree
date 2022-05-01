1b9d0042866c92d86a878f94ba91d87bfc2b66c3
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-01 18:37:03 +0200

    Install Eslint & Configure it with Prettier �

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!, Fadl");
+class Person {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  print(): string {
+    return `Hello ${this.name}`;
+  }
+}
+
+const person = new Person('Fadl');
+person.print();
