a08cee5643c3d4eb40e50f98a76d7921620ea6c1
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-01 21:39:48 +0200

    Added TS code that will be tested

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-class Person {
-  name: string;
+import arrays from './utilities/arrays.js';
+import numbers from './utilities/numbers.js';
+import strings from './utilities/strings.js';
 
-  constructor(name: string) {
-    this.name = name;
-  }
+const numArr = [3, 4, 5, 6];
+const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+const arrSum = arrays.addArr(numArr);
+const mixArr = arrays.concatArr(numArr, wordArr);
+const myNum = ('15' as unknown as number) % 2;
+const five = parseInt('5');
 
-  print(): string {
-    return `Hello ${this.name}`;
-  }
-}
-
-const person = new Person('Fadl');
-person.print();
+console.log(arrays.cut3(mixArr));
+console.log(numbers.sum(arrSum, myNum));
+console.log(strings.capitalize('the quick brown fox'));
+console.log(numbers.multiply(five, 8));
+console.log(arrays.lgNum(mixArr));
